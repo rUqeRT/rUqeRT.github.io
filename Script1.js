c2c1adcc4a99058973d99ba64bdcf56b79ac2960
rUqeRT <51176795+rUqeRT@users.noreply.github.com>
2019-05-30 11:34:39 +0200

    3636

--- Script1.js
+++ Script1.js
@@ -6,14 +6,15 @@ var scene = new THREE.Scene();
 var camera = new THREE.PerspectiveCamera( 75, window.innerWidth/window.innerHeight, 0.1, 1000 );
 
 var renderer = new THREE.WebGLRenderer();
+renderer.shadowMap.enabled = true;
+renderer.shadowMap.type = THREE.PCFSoftShadowMap;
 renderer.setSize( window.innerWidth, window.innerHeight );
 document.body.appendChild( renderer.domElement );
 
 var geometry = new THREE.BoxGeometry( 1, 1, 1 );
 var material = new THREE.MeshBasicMaterial( { color: coloro } );
 var cube = new THREE.Mesh( geometry, material );
-scene.add( cube );
-
+scene.add(cube);
 var loader = new THREE.FBXLoader();
 loader.load('NewDarkCitrus.fbx', function (object) {
     object.traverse(function (child) {
